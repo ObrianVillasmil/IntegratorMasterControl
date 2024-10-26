b0036416ff0c28e08be21934a9e9671f5c842d07
Obrian Villasmil <33008081+ObrianVillasmil@users.noreply.github.com>
2024-10-26 15:11:58 -0500

    updates

--- app/Models/WebHookUber.php
+++ app/Models/WebHookUber.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class WebHookUber extends Model
+class WebhookUber extends Model
 {
     use HasFactory;
 
     protected $fillable = ['data'];
-    
+
 }
