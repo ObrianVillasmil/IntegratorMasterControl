a0c60f5c1f17d79a8d379e23fb47a66ef0247480
Obrian Villasmil <33008081+ObrianVillasmil@users.noreply.github.com>
2024-10-25 21:44:22 -0500

    updates

--- app/Http/Controllers/UberNotificationController.php
+++ app/Http/Controllers/UberNotificationController.php
@@ -14,7 +14,7 @@ class UberNotificationController extends Controller
 
 
             //info($request->all());
-            info($request->toString());
+            info($request->__toString());
             //$json = json_encode($request->all());
             //info($json);
             $hmac = hash_hmac('sha256',$request->__toString(),'B_hWdS2sPQzZeckJHE06v1ryWHnDE3ByF0fN0D4A');
