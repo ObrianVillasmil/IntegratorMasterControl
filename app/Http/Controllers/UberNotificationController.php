ddb2a5331deddabebda8bcb05cbe3b27bfa86565
Obrian Villasmil <33008081+ObrianVillasmil@users.noreply.github.com>
2024-10-25 19:15:03 -0500

    updates

--- app/Http/Controllers/UberNotificationController.php
+++ app/Http/Controllers/UberNotificationController.php
@@ -9,9 +9,26 @@ class UberNotificationController extends Controller
     public function getNotification(Request $request)
     {
         $signatureUber =  $request->headers->get('X-Uber-Signature');
-        info($signatureUber);
-        info($request->all());
-        return response('',200);
+
+        if(isset($signatureUber) && $signatureUber != ''){
+
+
+            info($signatureUber);
+            info($request->all());
+
+
+           $hmac =  hash_hmac('sha256',json_encode($request->all()),'B_hWdS2sPQzZeckJHE06v1ryWHnDE3ByF0fN0D4A');
+
+           info($hmac);
+
+            return response('',200);
+
+        }else{
+
+            return response('',403);
+
+        }
+
 
     }
 }
