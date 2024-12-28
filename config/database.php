b7215fd4a0f7f920d1735019cdcedbf7bde2e245
Obrian Villasmil <33008081+ObrianVillasmil@users.noreply.github.com>
2024-12-28 13:35:21 -0500

    updates

--- config/database.php
+++ config/database.php
@@ -87,6 +87,20 @@ return [
             'sslmode' => 'prefer',
         ],
 
+        'topten_mc'  => [
+            'driver' => 'pgsql',
+            'host' => env('DB_HOST_TOPTEN', '127.0.0.1'),
+            'port' => env('DB_PORT_TOPTEN', '5432'),
+            'database' => env('DB_DATABASE_TOPTEN', 'forge'),
+            'username' => env('DB_USERNAME_TOPTEN', 'forge'),
+            'password' => env('DB_PASSWORD_TOPTEN', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
+
         'mipclocal' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST_MIPCLOCAL', '127.0.0.1'),
