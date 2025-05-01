e9989147cab72a3ae527686a5740ba24e0510679
obrian villasmil <villasmilobrian@gmai.com>
2025-05-01 10:32:25 -0500

    updates

--- app/Http/Controllers/PedidosYaWebhookController.php
+++ app/Http/Controllers/PedidosYaWebhookController.php
@@ -10,15 +10,19 @@ class PedidosYaWebhookController extends Controller
     {
         info("WEBHOOK RECEPCION DE PEDIDO PEDIDOS YA:\n");
 
-        info($request->header('Authorization')."\n");
-
-        info(print_r($request->all())."\n");
-
-        info($request->url()."\n");
-
-        info($request->path()."\n");
+        info(print_r($request->all(),true)."\n");
 
         info("Info recibida: \n\n ".$request->__toString());
+
+        $vendorId = explode('order/',$request->path())[1];
+
+        $jwt = $request->header('Authorization');
+
+        $token = trim(explode('Bearer ',$jwt)[1]);
+
+        $payload = json_decode(base64_decode(explode('.',$token)[1]));
+
+        info(print_r((array)$payload,true)."\n");
 
         return response("",200);
 
